621c2dbc91419bed34287ff010a56febd47fdbe0
Reem <reemgbr2@gmail.com>
2025-05-03 17:51:25 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import "./style.css";
+
+import "font-awesome/css/font-awesome.css";
+import Swiper from "swiper";
+import "swiper/css";
+import { Navigation, Pagination } from "swiper/modules";
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  modules: [Navigation],
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    991: {
+      slidesPerView: 2,
+    },
+    1320: {
+      slidesPerView: 4,
+    },
+  },
+
+  // Navigation arrows
+  //   navigation: {
+  //     nextEl: ".custom-button-next",
+  //     prevEl: ".custom-button-prev",
+  //   },
+});
+
+document.querySelector("#app").innerHTML = `
+
+ 
+`;
